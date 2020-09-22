3b77cd6aa03810bc902f8836824c696b6004da0a
David Larsen <davidplarsen@gmail.com>
2020-09-22 11:10:15 -0500

    Add class annotations for validation

--- DogGo/Models/Dog.cs
+++ DogGo/Models/Dog.cs
@@ -1,12 +1,22 @@
-﻿namespace DogGo.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DogGo.Models
 {
     public class Dog
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Every dog needs a name...")]
+        [MaxLength(35)]
         public string Name { get; set; }
+        [Required]
         public int OwnerId { get; set; }
+        [Required]
         public string Breed { get; set; }
         public string Notes { get; set; }
+        [Url]
+        [DisplayName("Picture URL")]
+
         public string ImageUrl { get; set; }
         public Owner Owner { get; set; }
     }
