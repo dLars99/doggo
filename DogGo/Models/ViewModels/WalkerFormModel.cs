9095df7d60641e86f781f089dc111b0c0dd58be1
David Larsen <davidplarsen@gmail.com>
2020-09-22 11:40:14 -0500

    Fill out Walker CRUD

---
+++ DogGo/Models/ViewModels/WalkerFormModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogGo.Models.ViewModels
+{
+    public class WalkerFormModel
+    {
+        public Walker Walker { get; set; }
+        public List<Neighborhood> Neighborhoods { get; set; }
+
+    }
+}
